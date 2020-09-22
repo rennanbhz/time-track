b2ad63bb10466cd24411c2a1ad149789b25a318d
Renan Mattos <renanmf@lnxcit021800.cit>
2020-09-22 16:50:08 -0300

    DEV-01-starting-application

---
+++ src/main/java/com/personalproject/timetrack/TimeTrackApplication.java
@@ -0,0 +1,13 @@
+package com.personalproject.timetrack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TimeTrackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TimeTrackApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/personalproject/timetrack/TimeTrackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.personalproject.timetrack;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TimeTrackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

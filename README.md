6e4b558f0f9ce50180c86d4b690a3b3c1d8f8de2
Renan Mattos <renanmf@lnxcit021800.cit>
2020-09-23 15:05:23 -0300

    Added Travis CI integration

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
+[![Build Status](https://travis-ci.org/rennanbhz/time-track.svg?branch=master)](https://travis-ci.org/rennanbhz/time-track)
 # time-track
--- src/main/java/com/personalproject/timetrack/TimeTrackApplication.java
+++ src/main/java/com/personalproject/timetrack/TimeTrackApplication.java
@@ -2,8 +2,9 @@ package com.personalproject.timetrack;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class })
 public class TimeTrackApplication {
 
 	public static void main(String[] args) {

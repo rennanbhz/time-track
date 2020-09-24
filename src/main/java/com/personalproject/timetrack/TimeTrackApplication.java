6f999b7161ed73deba19fcbefcf93e4f76a80fe5
Renan Mattos <rennanbhz@hotmail.com>
2020-09-23 22:34:19 -0300

    Added config and tests for profiles

--- src/main/java/com/personalproject/timetrack/TimeTrackApplication.java
+++ src/main/java/com/personalproject/timetrack/TimeTrackApplication.java
@@ -1,8 +1,11 @@
 package com.personalproject.timetrack;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication(exclude = {DataSourceAutoConfiguration.class })
 public class TimeTrackApplication {
@@ -11,4 +14,14 @@ public class TimeTrackApplication {
 		SpringApplication.run(TimeTrackApplication.class, args);
 	}
 
+
+	@Value("${pagination.quantity}")
+	private int quantityPerPages;
+
+	@Bean
+	public CommandLineRunner commandLineRunner() {
+		return args -> {
+			System.out.println("Quantity of elements per page = " + this.quantityPerPages);
+		};
+	}
 }
--- src/test/java/com/personalproject/timetrack/TimeTrackApplicationTests.java
+++ src/test/java/com/personalproject/timetrack/TimeTrackApplicationTests.java
@@ -1,13 +1,26 @@
 package com.personalproject.timetrack;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class TimeTrackApplicationTests {
+
+	@Value("${pagination.quantity}")
+	private int quantityPerPages;
 
 	@Test
 	void contextLoads() {
+	}
+
+	@Test
+	void testCurrentContext_shouldReturn_Context() {
+		assertEquals (100, quantityPerPages);
 	}
 
 }
